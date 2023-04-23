3b7ca97d3681b7931179b6a558d8eb8753052557
Elanilson de Jesus <elanilsonpp@hotmail.com>
2023-04-23 14:20:32 -0300

    Card

--- app/src/main/java/br/com/cashpago/exemplosjetpackcompose/Componentes/Card.kt
+++ app/src/main/java/br/com/cashpago/exemplosjetpackcompose/Componentes/Card.kt
@@ -2,13 +2,19 @@ package br.com.cashpago.exemplosjetpackcompose.Componentes
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +29,9 @@ fun card(){
     Scaffold(modifier = Modifier
         .fillMaxSize()
         .padding(10.dp)) {paddingValues ->
-
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues), contentAlignment = Alignment.Center){
             Card(
                 modifier = Modifier
                     .padding(paddingValues)
@@ -38,6 +46,8 @@ fun card(){
                     Text(text = "Oier")
                 }
             }
+        }
+
 
     }
 }
---
+++ app/src/main/java/br/com/cashpago/exemplosjetpackcompose/Componentes/CheckBox.kt
@@ -0,0 +1,2 @@
+package br.com.cashpago.exemplosjetpackcompose.Componentes
+
